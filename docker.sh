09b5a3bd32dccf564f64f98e5762cc162458acaf
Andre Ramaciotti <andre@ramaciotti.com>
2016-04-02 15:15:19 -0300

    Render drafts

--- docker.sh
+++ docker.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+export VERBOSE=--verbose
+
 docker run --rm --label=jekyll --volume=$(pwd):/srv/jekyll \
-    -it -p 127.0.0.1:4000:4000 jekyll/jekyll:pages
+    -it -p 127.0.0.1:4000:4000 jekyll/jekyll:pages \
+    jekyll serve --watch -H 0.0.0.0 --drafts
+
