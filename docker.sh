f5da757afbd1ed4e05751d95e9393280cda3f8f2
Andre Ramaciotti <andre@ramaciotti.com>
2016-04-16 19:24:28 -0300

    Change font and make headers purple

--- docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export VERBOSE=--verbose
-
-docker run --rm --label=jekyll --volume=$(pwd):/srv/jekyll \
-    -it -p 127.0.0.1:4000:4000 jekyll/jekyll:pages \
-    jekyll serve --watch -H 0.0.0.0 --drafts
-
